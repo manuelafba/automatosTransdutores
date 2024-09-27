88999f54da129ed5c2c47cdad12c68a288c1b1e8
Carolina de Lima Alves <129516662+carolinalimaal@users.noreply.github.com>
2024-09-27 11:50:15 -0300

    adicionando comentários

--- questao_3.py
+++ questao_3.py
@@ -1,9 +1,14 @@
+
 def verificar_cadeia(alfabeto_entrada, cadeia_entrada):
+    """
+    Função para verificar se a cadeia contém somente símbolos do alfabeto de entrada
+    """
     for simbolo in cadeia_entrada:
         if simbolo not in alfabeto_entrada:
             print('A cadeia contém símbolos que não pertencem ao alfabeto de entrada.')
-            return False
-    return True
+            return False # Caso tenha algum símbolo que não pertença ao alfabeto de entrada
+    return True # Caso a verificação valide a cadeia 
+
 
 def maquina_mealy(alfabeto_entrada, transicoes, transducoes, estado_inicial, cadeia_entrada):
     """
@@ -11,24 +16,33 @@ def maquina_mealy(alfabeto_entrada, transicoes, transducoes, estado_inicial, cad
     """
     cadeia_saida = ''
     estado_atual = estado_inicial
+    # Verifica a validade da cadeia de entrada em relação ao pertencimento dos símbolos ao alfabeto de entrada
     if verificar_cadeia(alfabeto_entrada, cadeia_entrada):
+        # Caso verdadeiro, a Máquina de Mealy irá realizar as transições e transduções
         for simbolo in cadeia_entrada:
             print('='*30)
             print(f'Estado atual:   \t{estado_atual}')
             print(f'Símbolo atual:  \t{simbolo}')
             print(f'Próximo estado: \t{transicoes[(estado_atual, simbolo)]}\n')
-
+            
+            # Atualiza a cadeia de saída com o resultado da função de transdução
             cadeia_saida += transducoes[(estado_atual, simbolo)]
             print(f'Cadeia de saída: \t{cadeia_saida}\n')
-
+            
+            # Atualiza o estado atual com o resultado da função de transição
             estado_atual = transicoes[(estado_atual, simbolo)]
 
 
+# Definição dos estados do transdutor
 estados = ['q0', 'q1', 'q2', 'q3', 'q4']
 
+# Definição alfabeto de entrada
 alfa_entrada = ['25', '50', '100']
+
+# Definição alfabeto de saída
 alfa_saida = ['0', '1']
 
+# Definição da função de transição no padrão: estado atual, símbolo -> próximo estado
 funcao_transicao = {
     ('q0', '25'):'q1',
     ('q0', '50'):'q2',
@@ -46,6 +60,8 @@ funcao_transicao = {
     ('q4', '50'):'q2',
     ('q4', '100'):'q4',
 }
+
+# Definição da função de transdução no padrão: estado atual, símbolo -> símbolo para a cadeia de saída
 funcao_transducao = {
     ('q0', '25'):'0',
     ('q0', '50'):'0',
@@ -64,49 +80,8 @@ funcao_transducao = {
     ('q4', '100'):'1',
 }
 
+# Definição do estado inicial
 estado_inicial = 'q0'
 
+# Definição dos estados finais
 estados_finais = ['q0', 'q1', 'q2', 'q3', 'q4']
-
-
-
-moeda = 0
-print('Moedas aceita -> 25 ou 50 ou 100')
-entrada = []
-while True:
-    moeda = input('Insira uma moeda (0 para sair): ')
-    if moeda == '0':
-        break
-    entrada.append(moeda)
-
-
-maquina_mealy(alfa_entrada, funcao_transicao, funcao_transducao, estado_inicial, entrada)
-
-
-testes = [
-    ['50', '50', '100', '100', '50', '25', '25', '100', '25', '25', '100', '50', '25', '100', '50'],
-    ['100', '50', '100', '25', '25', '100', '25', '50', '25', '100', '50', '25', '25', '100', '50'],
-    ['25', '100', '25', '50', '100', '25', '50', '100', '25', '50', '100', '50', '25', '100', '50'],
-    ['50', '25', '25', '50', '25', '100', '100', '100', '25', '50', '25', '50', '100', '25', '50'],
-    ['100', '100', '25', '50', '50', '25', '25', '50', '25', '100', '100', '50', '25', '50', '100'],
-    ['50', '50', '25', '100', '100', '100', '50', '25', '25', '50', '25', '100', '25', '50', '100'],
-    ['100', '25', '50', '25', '50', '100', '25', '100', '50', '100', '25', '25', '50', '100', '25'],
-    ['25', '50', '100', '100', '25', '50', '25', '100', '50', '25', '100', '25', '50', '100', '25'],
-    ['50', '100', '100', '25', '50', '25', '100', '50', '25', '25', '100', '25', '50', '50', '100'],
-    ['25', '50', '25', '100', '25', '100', '100', '50', '50', '100', '50', '25', '100', '25', '25'],
-    ['100', '100', '50', '25', '50', '100', '25', '25', '50', '100', '25', '100', '25', '50', '50'],
-    ['50', '100', '25', '50', '25', '100', '50', '25', '100', '25', '50', '100', '100', '50', '25'],
-    ['100', '25', '50', '25', '25', '100', '50', '100', '25', '100', '50', '25', '50', '25', '100'],
-    ['25', '50', '100', '100', '25', '50', '100', '25', '50', '25', '100', '25', '50', '50', '100'],
-    ['100', '50', '100', '25', '25', '50', '100', '25', '50', '25', '50', '100', '25', '100', '25'],
-]
-
-contador = 1
-for entrada in testes:
-    print(f'Entrada {contador}\n')
-    maquina_mealy(alfa_entrada, funcao_transicao, funcao_transducao, estado_inicial, entrada)
-    contador += 1
-    print('x='*30)
-
-
-
